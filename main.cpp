c9b2395c38e89ffa30b7cfe72a30554deb5c8bcc
eychen2 <echen963@gmail.com>
2021-10-18 17:25:18 -0400

    wrote initial user input code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <bits/stdc++.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hi, welcome to tic-tac-toe!" << std::endl;
+    int rows=0;
+    int columns=0;
+    int k=INT_MAX;
+    std::string cheese;
+    std::cout<<std::endl;
+    std::cout <<"You can choose whether to play versus another person or an AI."<<std::endl;
+    std::cout<<"Type \"Person\" to play against a person and \"AI\" to play against an AI"<<std::endl;
+    //Will work on AI functionality later, for now will only be dealing with player v player//
+    bool gameOver=false;
+    bool playerTurn=true;
+    bool AI=false;
+    std::cout<<"How many rows do you want to play with?"<<std::endl;
+    std::cin>>rows;
+    std::cout<<"How many columns do you want to play with?"<<std::endl;
+    std::cin>>columns;
+    std::cout<<"What \"In a row\" do you want to play with"<<std::endl;
+    while(k>rows&&k>columns)
+    {
+        std::cin>>k;
+        if(k>rows&&k>columns)
+        {
+            std::cout << "Not a valid \"In a row\" input."
+                         "It must be less than or equal to the maximum of rows and columns" << std::endl;
+            std::cout << "The number of rows is " << rows << " and the number of columns is " << columns << std::endl;
+        }
+    }
+
     return 0;
 }
